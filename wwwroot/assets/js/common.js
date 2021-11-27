14f5cade96bc3bdb11e81049100be41233a0e335
jamilligioielli <jamilligioielli@outlook.com>
2021-11-27 01:31:42 -0300

    Finalização do layout da página inicial [Completo]

--- wwwroot/assets/js/common.js
+++ wwwroot/assets/js/common.js
@@ -1,17 +1,3 @@
-﻿// Card jQuery
-// $(".section .owl-carousel .card").hover(
-//     function () {
-//         $('.owl-carousel .card #cart').css({
-// 					display: 'block',
-// 					opacity: '1',
-// 					transition: '2s'
-// 				})
-//     },
-//     function () {
-//         $('.owl-carousel .card #cart').css({ display: 'none', opacity: '0' })
-//     }
-// )
+﻿
 
-$('document').ready(function () {
-    $('.section .owl-carousel .owl-nav').removeClass('disabled')
-})
+
